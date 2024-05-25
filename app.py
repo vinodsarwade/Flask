a0e7fe9158ad314581bdd406ae223426dca1d7a3
vinodsarwade <vinodsarwade7@gmail.com>
2024-05-25 20:48:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/Home") #here we handling two routes for single function
+def home():
+    return "<h1> Home page</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>about page</h1>"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
